1e0714b42c85d1d65e8f0b1ae0b19d85bddbd6fe
Michael Fudge <mafudge@syr.edu>
2015-12-01 16:52:46 -0500

    Got hosts setup working in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# setup
-echo  "Get Centos 6.7 Image"
+# hdp-docker setup
+
+# todo apt-get upate
+# todo install docker ???
+
+echo  "*** Get Centos 6.7 Image ***"
 docker pull centos:6.7
-echo "Prepare HDP Base Node"
+
+echo "*** Prepare HDP Base Node ***"
 docker build -t ischool/hdpbase:v1 ./hdp-node
 
+echo "*** Setup Hosts File for hdp-docker ***"
+txtsrch="# hdp-docker hosts"
+txtfind=`grep "$txtsrch" /etc/hosts`
+if  [ "$txtsrch" != "$txtfind" ]
+then
+	echo "    Adding Hosts...."
+	cat data/hosts.add >> /etc/hosts
+else
+	echo "    Hosts alread exist"
+fi 
